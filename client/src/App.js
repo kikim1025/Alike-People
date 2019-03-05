5ac18e8dca68f3c82d741ed7efd8796453ab454e
Ki <kikim1025@gmail.com>
2019-03-05 02:18:24 -0500

    added Survey and Home react components

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import Survey from './components/Survey';
+import HomePage from './components/Home';
 import './App.css';
 
 class App extends Component {
   state = {
-
+    isPageSurvey: false,
+    
   }
 
 
